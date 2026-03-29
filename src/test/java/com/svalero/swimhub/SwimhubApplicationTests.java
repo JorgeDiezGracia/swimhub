a55a2b9f81ddb4780322b2c5ea3820f30e692cb3
JorgeDiezGracia <diezgraciajorge@gmail.com>
2026-03-30 00:29:21 +0200

    estructura inicial del proyecto y dependencias

---
+++ src/test/java/com/svalero/swimhub/SwimhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svalero.swimhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwimhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
